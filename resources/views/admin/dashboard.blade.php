a864af550f18b5476f47633dea972f53d09d9953
ahmetceyran <ahmetceyran00@gmail.com>
2023-12-01 22:50:04 +0300

    Add and Edit Admin Template

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,24 @@
-<x-app-layout>
+<!DOCTYPE html>
+<html>
+  <head> 
+    
+    @include('admin.css')
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
+  </head>
+  <body>
+    
+    @include('admin.header')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in ad admin!") }}
-                </div>
-            </div>
-        </div>
-    </div>
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+      
+        @include('admin.sidebar')
 
-</x-app-layout>
+      <!-- Sidebar Navigation end-->
+      
+        @include('admin.body')
+
+        @include('admin.footer')
+
+  </body>
+</html>
